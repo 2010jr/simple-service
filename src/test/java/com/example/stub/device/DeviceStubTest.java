992a404ae6ec42cc90ed054ac28d5ef9a2271b8e
Jun Kusahana <junkusa@jp.ibm.com>
2018-01-31 13:56:31 +0900

    Sample

--- src/test/java/com/example/stub/device/DeviceStubTest.java
+++ src/test/java/com/example/stub/device/DeviceStubTest.java
@@ -1,7 +1,6 @@
 package com.example.stub.device;
 
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
 
@@ -12,13 +11,5 @@ public class DeviceStubTest {
 
     @Test
     public void test() throws IOException, InterruptedException, AWSIotException, AWSIotTimeoutException {
-        DeviceStub stub = new DeviceStub("a33hoix1t55rlh.iot.us-east-1.amazonaws.com", "device_stub1",
-                "device/c12732a6b6-certificate.pem.crt.txt", "device/c12732a6b6-private.pem.key");
-        stub.start();
-        stub.subscribe("mytopic/device_stub1");
-
-        TimeUnit.SECONDS.sleep(100);
-
-        stub.stop();
     }
 }
